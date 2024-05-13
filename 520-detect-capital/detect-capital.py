ddd04f7fe9527ace10ad2f37011cdfbc18d6a1d9
Sreyas Namboothiri S <148409184+seyass@users.noreply.github.com>
2024-05-13 12:50:28 +0530

    Time: 43 ms (8.98%) | Memory: 16.5 MB (88.51%) - LeetSync

---
+++ 520-detect-capital/detect-capital.py
@@ -0,0 +1,7 @@
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        if word.isupper() or word.islower() or word.istitle():
+            return True
+        else:
+            return False
+        
