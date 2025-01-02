823635944f60f90cb8553d1d6e5e42984a0857cd
Sreyas Namboothiri S <148409184+sreyasnamboothiris@users.noreply.github.com>
2025-01-02 14:03:24 +0530

    Time: 4 ms (9.83%) | Memory: 17.8 MB (11.80%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        item = {
+            '(':')',
+            '[':']',
+            '{':'}'
+        }
+        # declare stack
+        stack = []
+        for character in s:# checking the charcter in the item(dict)
+            if character in item:
+                # push the element into stack if the charcter is key.
+                stack.append(character) 
+            else:
+                if len(stack):
+                    if item[stack[-1]] == character:
+                        stack.pop()
+                    else:
+                        return False
+                else:
+                    return False
+        if len(stack):
+            return False
+        else:
+            return True
+                    
+        return True
+
+
+
+        
