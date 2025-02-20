7614f40470fd758b4e3bc8f2e5fce762ea0dead6
Sreyas Namboothiri S <148409184+sreyasnamboothiris@users.noreply.github.com>
2025-02-20 09:25:53 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (14.34%) - LeetSync

---
+++ 551-student-attendance-record-i/student-attendance-record-i.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkRecord(self, s: str) -> bool:
+        if 'LLL' in s:
+            return False
+        if s.count('A') >= 2:
+            return False
+        return True
+        
