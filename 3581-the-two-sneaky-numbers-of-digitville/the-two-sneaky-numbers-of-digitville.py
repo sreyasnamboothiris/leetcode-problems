1412c1e3a0720d30f4214647b9781979bfc88fee
Sreyas Namboothiri S <148409184+sreyasnamboothiris@users.noreply.github.com>
2025-02-28 09:54:58 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (68.28%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.py
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.py
@@ -2,9 +2,12 @@ class Solution:
     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
 
         a = []
+        d = {}
         for i in nums:
-            if i not in a:
-                if nums.count(i) == 2:
-                    a.append(i)
+            if i in d:
+                d[i] += 1
+                a.append(i)
+            else:
+                d[i] = 1
         return a
         
